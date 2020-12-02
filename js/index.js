716cd9c4030f84cb9cefe411043ee6bcd933ccf7
Zimmmmm <57937268+Zimmmmm@users.noreply.github.com>
2020-12-02 22:26:07 +0800

    入门页完成

--- js/index.js
+++ js/index.js
@@ -10,6 +10,7 @@ window.addEventListener('load',function() {
     });
 
     // 进入网页字体变色
+<<<<<<< Updated upstream
     zim.style.color = 'white';
     bot.style.color = 'white';
     buildTime.style.color = 'white';
@@ -20,6 +21,18 @@ window.addEventListener('load',function() {
         var rgbPara = 255-(scrollY-target)*5.1;
         obj.style.transition = 'none';
         obj.style.color = 'rgb(' + rgbPara + ','+ rgbPara + ','+ rgbPara + ')'
+=======
+    zim.style.opacity = '1';
+    bot.style.opacity = '1';
+    buildTime.style.opacity = '1';
+
+    // 通过修改字体透明度 在网页下滑时让字符显示 / 隐藏
+    // 滑动变色函数封装
+    function colorChange(obj, target){
+        var opaPara = 1-(scrollY-target)*0.02;
+        obj.style.transition = 'none';
+        obj.style.opacity = opaPara;
+>>>>>>> Stashed changes
     }
     //  网页滑动字体颜色变化
     window.addEventListener('scroll',function() {
