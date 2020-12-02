0f00db1c81421c6b8aeac1664065b974ba25e914
Zimmmmm <57937268+Zimmmmm@users.noreply.github.com>
2020-12-02 22:27:39 +0800

    前页更新

--- js/index.js
+++ js/index.js
@@ -10,18 +10,6 @@ window.addEventListener('load',function() {
     });
 
     // 进入网页字体变色
-<<<<<<< Updated upstream
-    zim.style.color = 'white';
-    bot.style.color = 'white';
-    buildTime.style.color = 'white';
-
-    // 网页下滑字体变色
-    // 滑动变色函数封装
-    function colorChange(obj, target){
-        var rgbPara = 255-(scrollY-target)*5.1;
-        obj.style.transition = 'none';
-        obj.style.color = 'rgb(' + rgbPara + ','+ rgbPara + ','+ rgbPara + ')'
-=======
     zim.style.opacity = '1';
     bot.style.opacity = '1';
     buildTime.style.opacity = '1';
@@ -32,7 +20,6 @@ window.addEventListener('load',function() {
         var opaPara = 1-(scrollY-target)*0.02;
         obj.style.transition = 'none';
         obj.style.opacity = opaPara;
->>>>>>> Stashed changes
     }
     //  网页滑动字体颜色变化
     window.addEventListener('scroll',function() {
